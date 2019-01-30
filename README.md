c16b33c2cb83b095002fd8a79898986deed93ac8
Aarron Lee <aclee91@gmail.com>
2019-01-30 02:05:49 -0500

    bundled files for github pages

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cc_guide
+
+see https://github.com/aarron-lee/climate_change_pwa
+
---
+++ precache-manifest.ee9c5762c069d79a0e83916f245c3e3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "914b1f7424351bc804366fde0c3b178c",
+    "url": "static/transport.jpg"
+  },
+  {
+    "revision": "adb56e7a52b906fa41c1d225a10a10ac",
+    "url": "static/home.jpg"
+  },
+  {
+    "revision": "4c48b5c1748eef3d8039409ec824577e",
+    "url": "static/gases_chart.png"
+  },
+  {
+    "revision": "471275b818db66d83dc8d0a5dd5c7356",
+    "url": "static/food_waste.jpg"
+  },
+  {
+    "revision": "e4e773fb626ce0994f954eb7c5ddb0de",
+    "url": "static/food_chart.png"
+  },
+  {
+    "revision": "7574f2ed3cdfdd3142307af6309c4ad7",
+    "url": "index.html"
+  },
+  {
+    "revision": "8710b3be32c1a87d3d57",
+    "url": "assets/vendors.251026e8567e079d813d.js"
+  },
+  {
+    "revision": "a05df54f8ae64f58104f",
+    "url": "assets/runtime.77b2c6bc724e4f08d73f.js"
+  },
+  {
+    "revision": "fced371c51f4e29e6b72",
+    "url": "assets/app.997a36f28a2f0be40d7c.js"
+  }
+];
